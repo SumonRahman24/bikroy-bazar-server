5025bba488695290d165ddae15137d3e08b0bfcf
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-10-31 13:57:38 +0600

    some featues added

--- index.js
+++ index.js
@@ -1,17 +1,123 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
+const port = process.env.PORT || 4000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174"],
+    Credential: true,
+  })
+);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello express server");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bikroy-bazar.njiturm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+async function run() {
+  try {
+    await client.connect();
+
+    const productCollection = client.db("bikroyDB").collection("products");
+    const cartCollection = client.db("bikroyDB").collection("cart");
+
+    // [auth] related apis route-- jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiredIn: "24h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    //products route apis
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { product_name: 1, price: 1, image_url: 1 },
+      };
+      const result = await productCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // cart apis route
+    app.get("/cart", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      console.log(query);
+
+      const result = await cartCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const cartInfoData = req.body;
+      const result = await cartCollection.insertOne(cartInfoData);
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateConfirmData = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateConfirmData.status,
+        },
+      };
+      const result = await cartCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // home apis route
+    app.get("/", (req, res) => {
+      res.send("hello express server");
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`server is running successfully at http://${port}`);
+  console.log(`server is running at http://${port}`);
 });
